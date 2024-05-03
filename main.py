3a1dbc875531478b9cc89aff09b164c7141d9849
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-05-03 10:24:30 +0530

    created an engine that will speak to users

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import speech_recognition as sr
 import pyttsx3
 
 listener = sr.Recognizer()
+engine=pyttsx3.init()
 try:
         with sr.Microphone() as source:
                 print('listening...')
