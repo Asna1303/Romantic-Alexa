ac2ba3d3192f27bf227e1908955bd2270ab016e8
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-05-03 10:10:18 +0530

    given a condition using if : whether alexa is mentioned in the command, then print

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import speech_recognition as sr
 listener = sr.Recognizer()
 try:
         with sr.Microphone() as source:
+                print('listening...')
                 voice = listener.listen(source)
                 command = listener.recognize_google(voice)
+                command=command.lower()
+                if 'alexa' in command:
+                        print(command)
 except:
         pass
