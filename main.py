b91d74cf91d1419e4cb0b102133cb8f52c3c70bd
Asna T H <79138153+Asna1303@users.noreply.github.com>
2024-05-04 08:34:46 +0530

    Improved command processing by removing the trigger word 'alexa' from the recognized command string.

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def take_command():
                         command = listener.recognize_google(voice)
                         command = command.lower()
                         if 'alexa' in command:
+                                command = command.replace('alexa', '')
                                 print(command)
         except:
                 pass
